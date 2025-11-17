0f04847609e70e7caf8acbfb4d8bc927fcc82d48
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2025-11-17 21:45:57 +0500

    Added registration routine

--- backend/app/auth/controllers.py
+++ backend/app/auth/controllers.py
@@ -6,12 +6,83 @@ import app
 from app import db
 from app import config_
 from models.models import Users
+from models.models import Cities
+from models.models import Countries
 import secrets
 import re
-from utils.utils import check_password, encode_jwt, is_valid_auth_token, get_auth_token, decode_jwt
+from utils.utils import check_password, encode_jwt, is_valid_auth_token, get_auth_token, decode_jwt, hash_password
 
 mod_auth = Blueprint('auth', __name__, url_prefix='/auth')
 
+@mod_auth.route("/register/", methods=["POST"])
+def register():
+    data = request.get_json(force=True)
+    if not data:
+        return jsonify({
+            "success": False
+        })
+    username = data.get("username", None)
+    email = data.get("email", None)
+    phone = data.get("phone", None)
+    first_name = data.get("first_name", None)
+    last_name = data.get("last_name", None)
+    street_address = data.get("street_address", None)
+    postal_code = data.get("postal_code", None)
+    city_code = data.get("city_code", None)
+    country_code = data.get("country_code", None)
+    password = data.get("password", None)
+    if not re.match(r"[a-zA-Z\._0-9]{1,20}@[A-Za-z]{1,10}\.[a-zA-Z]{1,8}", email):
+        return jsonify({
+            "success": False,
+            "reason": "Invalid email address"
+        })
+    if not re.match(r"\+[0-9]{10,20}", phone):
+        return jsonify({
+            "success": False,
+            "reason": "Invalid phone number"
+        })
+    if not re.match(r"[0-9]{4,10}", postal_code):
+        return jsonify({
+            "success": False,
+            "reason": "Invalid postal code"
+        })
+    salt = hexlify(os.urandom(32))
+    user = Users.query.filter_by(username=data.get("username", None)).first()
+    if not user:
+        return jsonify({
+            "success": False,
+            "reason": "User already exists"
+        })
+    country = Countries.query.filter_by(country_code=country_code).first()
+    if not country:
+        return jsonify({
+            "success": False,
+            "reason": "Invalid city"
+        })
+    city = Cities.query.filter_by(city_code=city_code, country_code=country_code).first()
+    if not city:
+        return jsonify({
+            "success": False,
+            "reason": "Invalid city"
+        })
+    user = Users()
+    user.username = username
+    user.password = hash_password(password, salt)
+    user.salt = salt
+    user.phone = phone
+    user.first_name = first_name
+    user.last_name = last_name
+    user.street_address = street_address
+    user.postal_code = postal_code
+    user.city_code = city_code
+    user.country_code = country_code
+    user.confirmed = False
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({
+            "success": True,
+            "reason": "Verification email was sent to your address"
+        })
 @mod_auth.route("/signin/", methods=["POST"])
 def signin():
     if request.method == "POST":
@@ -23,7 +94,7 @@ def signin():
         salt = hexlify(os.urandom(32))
         user = Users.query.filter_by(username=data.get("username", None)).first()
         
-        if user and check_password(data.get("password", "").encode("UTF-8"), user.salt.encode("UTF-8"), user.password.encode("UTF-8")):
+        if user and user.confirmed and check_password(data.get("password", "").encode("UTF-8"), user.salt.encode("UTF-8"), user.password.encode("UTF-8")):
             token = encode_jwt(user.username, user.password, salt.decode("UTF-8"), config_["SERVER_NONCE"], config_["JWT_VALIDITY_IN_DAYS"], config_["TOKEN_KEY"])
             resp = make_response(jsonify({
                 "success": True
@@ -58,6 +129,7 @@ def renew_token():
         return jsonify({
             "success": False
         })
+    
     if is_valid_auth_token(token, config_["SERVER_NONCE"], config_["TOKEN_KEY"]):
         token = encode_jwt(payload["subject"], salt.decode("UTF-8"), config_["SERVER_NONCE"], config_["JWT_VALIDITY_IN_DAYS"], config_["TOKEN_KEY"])
         resp = make_response(jsonify({
--- backend/models/models.py
+++ backend/models/models.py
@@ -1,13 +1,39 @@
 from app import db
 
+class Countries(db.Model):
+
+    __tablename__ = "Countries"
+    country_code   = db.Column(db.String(3), nullable=False, primary_key = True)
+    country        = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return '<Country %r>' % (self.country)
+    
+class Cities(db.Model):
+
+    __tablename__ = "Cities"
+    city_code      = db.Column(db.String(3), nullable=False, primary_key = True)
+    city           = db.Column(db.String(200), nullable=False)
+    country_code   = db.Column(db.String(3), nullable=False, primary_key = True)
+
+    def __repr__(self):
+        return '<Country %r>' % (self.country)
+
 class Users(db.Model):
 
     __tablename__ = "Users"
-    username  = db.Column(db.String(100), nullable=False, primary_key = True)
-    email     = db.Column(db.String(200), nullable=False)
-    password  = db.Column(db.String(200), nullable=False)
-    salt      = db.Column(db.String(100), nullable=False)
-    confirmed = db.Column(db.Boolean, nullable=False, default=False)
+    username       = db.Column(db.String(100), nullable=False, primary_key = True)
+    email          = db.Column(db.String(200), nullable=False)
+    phone          = db.Column(db.String(100), nullable=True)
+    first_name     = db.Column(db.String(200), nullable=True)
+    last_name      = db.Column(db.String(200), nullable=True)
+    street_address = db.Column(db.String(200), nullable=True)
+    postal_code    = db.Column(db.String(100), nullable=True)
+    city_code      = db.Column(db.String(10), nullable=False)
+    country_code   = db.Column(db.String(3), nullable=False)
+    password       = db.Column(db.String(200), nullable=False)
+    salt           = db.Column(db.String(100), nullable=False)
+    confirmed      = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return '<User %r>' % (self.username)
