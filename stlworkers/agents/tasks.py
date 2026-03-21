49e6026f81f0fa302efaa544132df2700e605eaa
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2026-03-21 19:31:13 +0500

    Store the computation results into the database

--- stlworkers/agents/tasks.py
+++ stlworkers/agents/tasks.py
@@ -6,7 +6,7 @@ import os
 from models import session, ObjectVersions
 
 @celery.task(bind=True, max_retries=3)
-def process_stl(self, file_path, object, version):
+def process_stl(self, file_path, object, version, owner):
     try:
         mesh = trimesh.load(file_path)
         volume = mesh.volume
@@ -18,6 +18,7 @@ def process_stl(self, file_path, object, version):
         counts = mesh.edges_unique_face_count
         non_manifold_edges = edges[counts != 2]
         is_edge_manifold = (mesh.edges_unique_face_count == 2).all()
+        is_watertight = mesh.is_watertight
         non_manifold_vertices = []
         for v_idx in range(len(mesh.vertices)):
             faces = mesh.vertex_faces[v_idx]
@@ -32,5 +33,28 @@ def process_stl(self, file_path, object, version):
         cog_x = cog[0]
         cog_y = cog[1]
         cog_z = cog[2]
+
+        obj = session.query(ObjectVersions).filter_by(object = object, version = version, owner = owner).first()
+        if not obj:
+            return
+        obj.surface_area = surface_area
+        obj.volume = volume
+        obj.cog_x = cog_x
+        obj.cog_y = cog_y
+        obj.cog_z = cog_z
+        obj.bb_x_l = bbox[0]
+        obj.bb_y_l = bbox[1]
+        obj.bb_z_l = bbox[2]
+        obj.bb_x_h = bbox[3]
+        obj.bb_y_h = bbox[4]
+        obj.bb_z_h  = bbox[5]
+        obj.is_water_tight = is_watertight
+        obj.number_of_facets = num_facets
+        obj.number_of_unique_verticies = unique_vertices
+        obj.is_edge_manifold = is_edge_manifold
+        obj.is_vertex_manifold = is_vertex_manifold
+
+        session.commit();
+    
     except Exception as e:
         raise self.retry(exc=e, countdown=10)
