16e350c56aebe3fee83b1679a2d7d467e1ffc9b5
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2026-03-21 16:27:11 +0500

    Fixed domain in cookie

--- backend/app/auth/controllers.py
+++ backend/app/auth/controllers.py
@@ -331,7 +331,7 @@ def renew_token():
             resp = make_response(jsonify({
                 "success": True
             }))
-            resp.set_cookie('token', token, max_age=24*60*60, httponly=True, secure=False, samesite='Lax')
+            resp.set_cookie('token', token, max_age=24*60*60, httponly=True, secure=False, samesite='Lax', domain="localhost")
         else:
             return jsonify({
                 "success": True
